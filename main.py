aea2450054836fe96522973be50c3c4df339168e
AKHILAADLA <167178769+AKHILAADLA@users.noreply.github.com>
2025-02-26 22:12:39 -0500

    Update main.py

--- main.py
+++ main.py
@@ -212,4 +212,4 @@ def tts_text_file(filename):
     return send_from_directory(TTS_TEXT_FOLDER, filename)
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=int(os.environ.get('PORT', 8080)))
+    app.run(debug=True)
